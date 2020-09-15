e20aa121c44736a25991b2e4c017e0f5b4a8c085
Entropie67 <olivier.elophe@gmail.com>
2020-09-15 13:00:47 +0200

    Initialisation de la séance NSI 1 du 15/09/2020

--- NSI 1/NSI1_fonctions_15092020.py
+++ NSI 1/NSI1_fonctions_15092020.py
@@ -1,19 +1,37 @@
 ####################################
 #           Les fonctions          #
 ####################################
-# NSI 1
-# M. Elophe
+# NSI 1 - M. Elophe                #
 ####################################
-
-import turtle
-
+import turtle # Pour l'une ou l'autre fonction
 # Voici trois exemples de fonctions.
 
-def damier(n: int, m: int) ->int:
+
+def damier(n: int, m: int) -> int:
     pass
+
 
 def perimetre_triangle(a: float, b: float, c: float) -> float:
     pass
 
-def trangle_bleu(l: int) -> None:
-    pass
+
+def triangle_bleu(l: int) -> None:
+    """ Procédure permettant de tracer un triangle bleu"""
+    t = turtle.Turtle()
+    t.fillcolor('blue')
+    t.begin_fill()
+    for i in range(3):
+        t.forward(100)
+        t.left(120)
+    t.end_fill()
+    turtle.exitonclick()
+
+# Une fonction qui ne retourne rien est appellée une procédure.
+# Ceci dit, en Python, None est retourné par défaut
+
+triangle_bleu(100)
+
+##############################
+#           EXERCICES        #
+##############################
+
