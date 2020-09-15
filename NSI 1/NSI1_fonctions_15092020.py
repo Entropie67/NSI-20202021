0697bfc7422d720ac293d1a2b2e752d6529242f5
Entropie67 <olivier.elophe@gmail.com>
2020-09-15 10:34:14 +0200

    Initialisation de la séance NSI 1 du 15/09/2020

---
+++ NSI 1/NSI1_fonctions_15092020.py
@@ -0,0 +1,19 @@
+####################################
+#           Les fonctions          #
+####################################
+# NSI 1
+# M. Elophe
+####################################
+
+import turtle
+
+# Voici trois exemples de fonctions.
+
+def damier(n: int, m: int) ->int:
+    pass
+
+def perimetre_triangle(a: float, b: float, c: float) -> float:
+    pass
+
+def trangle_bleu(l: int) -> None:
+    pass
