6981bb5f1bf25fb78a222eb449039796d0e85d35
Olivier <olivier.elophe@gmail.com>
2021-01-05 12:27:18 +0100

    déplacement par case

--- NSI 1/NSI1_pygame/game.py
+++ NSI 1/NSI1_pygame/game.py
@@ -5,9 +5,17 @@ pg.init()
 screen = pg.display.set_mode((600, 400), RESIZABLE)
 pg.display.set_caption('Mon premier jeu')
 fond = pg.image.load("images/fond.jpg").convert()
+
 screen.blit(fond, (0, 0))
 # Chargement et collage du personnage
-perso = pg.image.load("images/robot.png").convert_alpha()
+directions = ["bas", "droite", "haut", "gauche"]
+perso_images = []
+for direction in directions:
+    path = "images/mario_" + direction + ".gif"
+    perso = pg.image.load(path).convert_alpha()
+    perso_images.append(perso)
+
+
 perso = pg.transform.scale(perso, (32, 32))
 position_perso = perso.get_rect()
 screen.blit(perso, position_perso)
@@ -47,10 +55,32 @@ def move(structure, position, direction):
             print("Bim le mur")
             return position
         else:
-            pass
+            position[0] += 32
+            #position = position.move(32, 0)
+    if direction == "gauche":
+        if structure[ligne][colonne + 1] == "m":
+            print("Bim le mur")
+            return position
+        else:
+            position[0] += 32
+            #position = position.move(32, 0)
+    if direction == "bas":
+        if structure[ligne][colonne + 1] == "m":
+            print("Bim le mur")
+            return position
+        else:
+            position[0] += 32
+            #position = position.move(32, 0)
+    if direction == "haut":
+        if structure[ligne][colonne + 1] == "m":
+            print("Bim le mur")
+            return position
+        else:
+            position[0] += 32
+            #position = position.move(32, 0)
     print(f"Le personne se trouve actuellement en colonne {colonne} et ligne {ligne}")
     return position
-
+i= 0
 
 continuer = True
 while continuer:
@@ -64,19 +94,23 @@ while continuer:
                 print("Tu viens d'utiliser la touche entrée !")
             if event.key == K_RIGHT:
                 move(structure, position_perso, "droite")
-                position_perso = position_perso.move(32, 0)
+                i = 1
+                #position_perso = position_perso.move(32, 0)
             if event.key == K_LEFT:
                 position_perso = position_perso.move(-32, 0)
+                i = 3
             if event.key == K_DOWN:
                 position_perso = position_perso.move(0, 32)
+                i = 0
             if event.key == K_UP:
                 position_perso = position_perso.move(0, -32)
+                i = 2
             # ajouter les autres déplacements
     # screen.blit(fond, (0, 0))
     screen.fill((129, 199, 132))
     # Dessine un rectangle sans raison.
     pg.draw.rect(screen, (0, 0, 255), (50, 50, 50, 50))
     affiche_map(screen, structure)
-    screen.blit(perso, position_perso)
+    screen.blit(perso_images[i], position_perso)
 
     pg.display.flip()
--- NSI 1/NSI1_pygame/lecture_fichier.py
+++ NSI 1/NSI1_pygame/lecture_fichier.py
@@ -3,10 +3,11 @@ import pygame as pg
 from pygame.locals import *
 
 pg.init()
-TAILLE_CASE = 32
-screen = pg.display.set_mode((600, 400), RESIZABLE)
+TAILLE_CASE = 64
+TAILLE_SCREEN = (980, 680)
+screen = pg.display.set_mode(TAILLE_SCREEN, RESIZABLE)
 mur = pg.image.load("images/mur.jpg").convert_alpha()
-mur = pg.transform.scale(mur, (32, 32))
+mur = pg.transform.scale(mur, (TAILLE_CASE, TAILLE_CASE))
 
 
 with open("maps/map01", 'r') as file:
@@ -20,9 +21,9 @@ for ligne in structure:
     num_col = 0
     for case in ligne:
         if case == "m":
-            screen.blit(mur, (num_col * 32, num_ligne * 32))
-        elif case == "_":
-            pass
+            screen.blit(mur, (num_col * TAILLE_CASE, num_ligne * TAILLE_CASE))
+        elif case == "#":
+            pg.draw.rect(screen, (255, 0, 0), [num_col * TAILLE_CASE, num_ligne * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE])
         print(f" La case ({num_ligne}, {num_col}) contient un {case}")
         num_col += 1
     num_ligne += 1
